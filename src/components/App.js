70c149aa85f9a6e79efdc13aba67bc7f9bc69b5a
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2023-08-30 01:57:49 +0900

    Router 적용: 로그인 여부에 따라 Home, Auth 페이지 렌더링

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
+import AppRouter from "./Router";
+
 function App() {
-  return (
-    <div> App </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,23 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
 
 const AppRouter = () => {
+    // isLoggedIn : 로그인이 되어 있는지 확인하는 변수 (boolean)
+    // setIsLoggedIn : isLoggedIn의 값을 변경하는 함수
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    // 로그인 되어있으면 Home을, 그렇지 않다면 로그인 페이지인 Auth 컴포넌트를 렌더링하자
     return (
         <Router>
-            <Switch>
-                <Route />
-            </Switch>
+            <Routes>
+                {isLoggedIn ? (
+                    <Route path="/" element={<Home />} />
+                )   
+                 : (
+                    <Route path="/" element={<Auth />} />
+                )}
+            </Routes>
         </Router>
     )
 };

d888380979f2d0a0b92b29e8a0f3695037344106
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2023-08-29 21:34:28 +0900

    firebase 연결, npm run start

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBmYcaGefpmNxOZRQ85n0ZIU-Xu7imchY",
+    authDomain: "nwitter-ef18d.firebaseapp.com",
+    projectId: "nwitter-ef18d",
+    storageBucket: "nwitter-ef18d.appspot.com",
+    messagingSenderId: "800549679791",
+    appId: "1:800549679791:web:12b25e2576e06e9d085a5a"
+};
+
+export default firebase.initializeApp(firebaseConfig);
